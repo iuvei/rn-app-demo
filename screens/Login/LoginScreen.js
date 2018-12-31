4377d2f429c4015e75f91c948c3b45ae234da62d
wade <wadeflash123@gmail.com>
2018-12-31 17:36:04 +0800

    登录

--- screens/Login/LoginScreen.js
+++ screens/Login/LoginScreen.js
@@ -60,61 +60,72 @@ class LoginComponent extends Component {
 
     return (
       <ImageBackground source={require('../../assets/images/login_bg.jpg')} style={{width: '100%', height: '100%', alignItems: 'center'}}>
-        <View style={{paddingTop: 88}}>
-          <Image source={require('../../assets/images/login_logo.png')} style={{width: 135, height: 105, margin: 'auto'}}/>
-        </View>
-        <View style={styles.container}>
-          <InputItem
-            style={{height: 45, width: 280, backgroundColor: '#ffffff', borderRadius: 5, margin: 0, marginBottom: 18, marginLeft: 0}}
-            placeholder="请输入用户名"
-            value={j_username}
-            clear
-            labelNumber={2}
-            onChangeText={(j_username) => this.setState({
-              formData: Object.assign({}, this.state.formData, {j_username})
-            })}
-          >
-            <Icon name="account-book" size="32" color="#1789e6" />
-          </InputItem>
-          <InputItem
-            style={{height: 45, width: 280, backgroundColor: '#ffffff', borderRadius: 5, margin: 0, marginLeft: 0}}
-            placeholder="请输入登录密码"
-            value={j_password}
-            labelNumber={2}
-            extra={<Icon name="account-book" size="32" color="#1789e6" />}
-            secureTextEntry
-            onChangeText={(j_password) => this.setState({
-              formData: Object.assign({}, this.state.formData, {j_password})
-            })}
-          >
-            <Icon name="account-book" size="32" color="#1789e6" />
-          </InputItem>
-          <Flex style={{height: 38}}>
-            <Flex.Item style={{ paddingLeft: 4, paddingRight: 4 }}>
-              <Text style={{textAlign: 'right', color: '#ffffff'}}>忘记密码</Text>
-            </Flex.Item>
-            <View style={{ paddingLeft: 4, paddingRight: 4, width: 60 }}>
-              <Switch
-                  checked={this.state.rememberPwd}
-                  onChange={this.onSwitchChange}
-                />
+        <Flex direction="column">
+          <Flex.Item>
+            <View style={{paddingTop: 88}}>
+              <Image source={require('../../assets/images/login_logo.png')} style={{width: 135, height: 105, marginLeft: 'auto', marginRight: 'auto'}}/>
             </View>
-          </Flex>
-          <View style={{width: 280}}>
-            <Button
-              style={styles.btn}
-              onPress={() => this._toLogin()}
-              title=' 登 陆 '/>
+            <View style={styles.container}>
+              <InputItem
+                style={{height: 45, width: 280, backgroundColor: '#ffffff', borderRadius: 5, margin: 0, marginBottom: 18, marginLeft: 0}}
+                placeholder="请输入用户名"
+                value={j_username}
+                extra={<Icon name="account-book" size="32" color="#1789e6" />}
+                labelNumber={2}
+                onChangeText={(j_username) => this.setState({
+                  formData: Object.assign({}, this.state.formData, {j_username})
+                })}
+                onExtraClick={() => this.setState({
+                  formData: Object.assign({}, this.state.formData, {j_username: ''})
+                })}
+              >
+                <Icon name="account-book" size="32" color="#1789e6" />
+              </InputItem>
+              <InputItem
+                style={{height: 45, width: 280, backgroundColor: '#ffffff', borderRadius: 5, margin: 0, marginLeft: 0}}
+                placeholder="请输入登录密码"
+                value={j_password}
+                labelNumber={2}
+                type="password"
+                extra={<Icon name="account-book" size="32" color="#1789e6" />}
+                onChangeText={(j_password) => this.setState({
+                  formData: Object.assign({}, this.state.formData, {j_password})
+                })}
+              >
+                <Icon name="account-book" size="32" color="#1789e6" />
+              </InputItem>
+              <Flex style={{height: 38}}>
+                <Flex.Item style={{ paddingLeft: 4, paddingRight: 4 }}>
+                  <Text style={{textAlign: 'right', color: '#ffffff'}}>忘记密码</Text>
+                </Flex.Item>
+                <View style={{ paddingLeft: 4, paddingRight: 4, width: 60 }}>
+                  <Switch
+                      checked={this.state.rememberPwd}
+                      onChange={this.onSwitchChange}
+                    />
+                </View>
+              </Flex>
+              <View style={{width: 280}}>
+                <Button
+                  style={styles.btn}
+                  onPress={() => this._toLogin()}
+                  title=' 登 陆 '/>
+              </View>
+              <Flex style={{width: 280, marginTop: 25}}>
+                <Flex.Item style={{ paddingLeft: 4, paddingRight: 4 }}>
+                  <Text style={{color: '#ffffff'}}>移动端下载</Text>
+                </Flex.Item>
+                <Flex.Item style={{ paddingLeft: 4, paddingRight: 4 }}>
+                  <Text style={{textAlign: 'right', color: '#ffffff'}}>线路检测</Text>
+                </Flex.Item>
+              </Flex>
+            </View>
+          </Flex.Item>
+          <View style={{height: 40, width: 280, flexDirection: 'row'}}>
+            <Text style={{color: '#ffffff', flex: 1}}>在线客服</Text>
+            <Text style={{textAlign: 'right', color: '#ffffff', flex: 1}}>1.11版正式发布</Text>
           </View>
-          <Flex style={{width: 280, marginTop: 25}}>
-            <Flex.Item style={{ paddingLeft: 4, paddingRight: 4 }}>
-              <Text style={{color: '#ffffff'}}>移动端下载</Text>
-            </Flex.Item>
-            <Flex.Item style={{ paddingLeft: 4, paddingRight: 4 }}>
-              <Text style={{textAlign: 'right', color: '#ffffff'}}>线路检测</Text>
-            </Flex.Item>
-          </Flex>
-        </View>
+        </Flex>
       </ImageBackground>
     )
   }
@@ -126,7 +137,6 @@ const styles = StyleSheet.create({
     width: 310,
     height: 290,
     backgroundColor: '#0a84db',
-    margin: 'auto',
     marginTop: 39,
     borderRadius: 7,
     paddingTop: 38,
