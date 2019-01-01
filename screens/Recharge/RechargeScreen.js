9ff474adab4559616c8f3e4488d886048cb6906b
wade <wadeflash123@gmail.com>
2019-01-01 16:58:14 +0800

    虚拟币充值

--- screens/Recharge/RechargeScreen.js
+++ screens/Recharge/RechargeScreen.js
@@ -8,6 +8,7 @@ const data = require('./data.json')
 import {MyIconFont} from '../../components/MyIconFont'
 import {RechargeChannelIconMap} from '../../constants/glyphMapHex'
 import {setActiveAccount} from '../../actions/common'
+import Header from '../../components/Header'
 
 const RadioItem = Radio.RadioItem;
 
@@ -24,7 +25,10 @@ class RechargeScreen extends React.Component {
       activeSections: [0],
       recharge: {},
       channelRealObj: {},
-      activeAccount: {}
+      // activeAccount: {}
+      virtualAccounts: [],
+      activeTabIndex: 0,
+      minRechargeMoney: 50
     };
     this.onOpenChange = isOpen => {
       /* tslint:disable: no-console */
@@ -39,6 +43,7 @@ class RechargeScreen extends React.Component {
         console.log('recharge', recharge)
         // 人民币渠道集合
         let channelRealObj = {}
+        let realAccounts = []
         Object.keys(recharge).forEach((keyTitle) => {
           if (keyTitle !== 'virtual') {
             Object.keys(recharge[keyTitle]).forEach((infomap) => {
@@ -51,6 +56,7 @@ class RechargeScreen extends React.Component {
                         this.props.setActiveAccount(recharge[keyTitle][infomap][channelReal][i]);
                       }
                       recharge[keyTitle][infomap][channelReal][i]['local_id'] =  channelReal + '_' + i + '_' + new Date().getTime()
+                      realAccounts.push(recharge[keyTitle][infomap][channelReal][i])
                     }
                     channelRealObj[channelReal] = recharge[keyTitle][infomap][channelReal]
                   }
@@ -59,8 +65,23 @@ class RechargeScreen extends React.Component {
             })
           }
         })
+        // 数字货币充值渠道集合
+        let virtualAccounts = []
+        if (recharge && recharge.virtual && recharge.virtual.virtualInfoMap) {
+          let channelVirtual = ''
+          for (channelVirtual in recharge.virtual.virtualInfoMap) {
+            if (recharge.virtual.virtualInfoMap.hasOwnProperty(channelVirtual)) {
+              recharge.virtual.virtualInfoMap[channelVirtual].forEach((accountVirtual, idx) => {
+                accountVirtual['local_id'] = channelVirtual + '_' + idx + '_' + new Date().getTime()
+                virtualAccounts.push(accountVirtual)
+              })
+            }
+          }
+        }
         console.log('channelRealObj', channelRealObj)
         this.setState({
+          virtualAccounts: [].concat(virtualAccounts),
+          realAccounts: [].concat(realAccounts),
           channelRealObj: Object.assign({}, channelRealObj),
           recharge: Object.assign({}, recharge)
         })
@@ -120,8 +141,21 @@ class RechargeScreen extends React.Component {
     );
   };
 
+  channelTabsChange = (tab, index) => {
+    console.log(tab, index, this.state.virtualAccounts)
+    this.setState({
+      activeTabIndex: index,
+      minRechargeMoney: index === 0 ? 50 : 100
+    })
+    if (index === 1) {
+      this.props.setActiveAccount(this.state.virtualAccounts[0])
+    } else {
+      this.props.setActiveAccount(this.state.realAccounts[0])
+    }
+  }
+
   render() {
-    let {channelRealObj} = this.state
+    let {channelRealObj, activeTabIndex, virtualAccounts, minRechargeMoney} = this.state
     let {activeAccount} = this.props
     const sidebar = (
       <ScrollView style={[styles.container]}>
@@ -142,6 +176,38 @@ class RechargeScreen extends React.Component {
         }
       </ScrollView>
     );
+    const sidebarVirtual = (
+      <ScrollView style={[styles.container]}>
+        <List>
+          {
+            virtualAccounts.map((item, index) => {
+              return <RadioItem
+                multipleLine
+                name="rechargeChannels"
+                checked={this.props.activeAccount.local_id === item.local_id}
+                key={item.payChannelAlias + index}
+                onChange={event => {
+                  if (event.target.checked) {
+                    this.props.setActiveAccount(item);
+                  }
+                }}>
+                <View
+                  style={{
+                    flexDirection: 'row',
+                    justifyContent: 'space-between',
+                    alignItems: 'center',
+                    height: 30,
+                    paddingLeft: 10
+                  }}
+                >
+                  <MyIconFont name={'icon_'+RechargeChannelIconMap[String(activeAccount.coinCode).toLowerCase()]} size={30}/>
+                </View>
+              </RadioItem>
+            })
+          }
+        </List>
+      </ScrollView>
+    )
     const tabs = [
       { title: '人民币支付' },
       { title: '币宝数字货币支付' }
@@ -154,10 +220,19 @@ class RechargeScreen extends React.Component {
       flexDirection: 'row',
       flex: 1
     };
+    const infoDesc = (
+      <View style={{paddingLeft: 12, paddingRight: 12}}>
+        <Text style={{color: '#a4a4a4', lineHeight: 25}}>充值金额：单笔最低充值金额为 <Text style={{color: '#f15a23'}}>{minRechargeMoney}</Text> 元{activeAccount.signleLimit > 0 ? <Text>, 最高 <Text style={{color: '#f15a23'}}>{activeAccount.signleLimit}</Text> 元</Text> : null}</Text>
+        {activeAccount.feeRate > 0 ? <Text style={{color: '#a4a4a4', lineHeight: 25}}>充值手续费费率 <Text style={{color: '#f15a23'}}>{ activeAccount.feeRate || 0 }%</Text></Text> : null}
+        {activeAccount.dayLimit > 0 ? <Text style={{color: '#a4a4a4', lineHeight: 25}}>充值金额：单日最高 <Text style={{color: '#f15a23'}}>{ activeAccount.dayLimit }</Text> 元</Text> : null}
+        <Text style={{color: '#a4a4a4', lineHeight: 25}}>充值限时：请在 <Text style={{color: '#f15a23'}}>30</Text> 分钟内完成充值</Text>
+      </View>
+    )
     return (
       <View style={styles.container}>
+        <Header hideLeft={true}/>
         <Drawer
-          sidebar={sidebar}
+          sidebar={activeTabIndex === 0 ? sidebar : sidebarVirtual}
           position="right"
           open={false}
           drawerRef={el => (this.drawer = el)}
@@ -165,31 +240,26 @@ class RechargeScreen extends React.Component {
           drawerBackgroundColor="#ccc"
         >
           <View style={{ flex: 1 }}>
-            <Tabs tabs={tabs}>
-              <View style={style}>
+            <Tabs tabs={tabs} onChange={this.channelTabsChange}>
+              <View>
                 <List style={{width: '100%'}}>
                   <List.Item arrow="horizontal" onPress={() => this.drawer && this.drawer.openDrawer()}>
                     {activeAccount.bankCode ? <MyIconFont name={'icon_'+RechargeChannelIconMap[activeAccount.bankCode]} size={30}/> : null}
                   </List.Item>
                 </List>
+                {infoDesc}
+              </View>
+              <View>
+                <List style={{width: '100%'}}>
+                  <List.Item arrow="horizontal" onPress={() => this.drawer && this.drawer.openDrawer()}>
+                    {activeAccount.coinCode ? <MyIconFont name={'icon_'+RechargeChannelIconMap[String(activeAccount.coinCode).toLowerCase()]} size={30}/> : null}
+                  </List.Item>
+                </List>
+                {infoDesc}
               </View>
             </Tabs>
           </View>
         </Drawer>
-        {/* <Provider>
-          <List>
-            <Picker
-              data={data}
-              cols={3}
-              value={this.state.value}
-              onChange={this.onChange}
-            >
-              <List.Item arrow="horizontal" onPress={this.onPress}>
-                省市选择
-              </List.Item>
-            </Picker>
-          </List>
-        </Provider> */}
       </View>
     )
   }
