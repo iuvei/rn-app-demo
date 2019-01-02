3b3cda95b1495c4fa5bcee72d0c58d9d3c7d32a3
ddi <ddili6599@gmail.com>
2019-01-02 17:19:38 +0800

    首页

--- actions/common.js
+++ actions/common.js
@@ -1,5 +1,45 @@
 import { getSysLottery } from "../api/lottery";
 import { createAction } from "redux-actions";
+import { AsyncStorage } from "react-native";
+const usual = [
+  {
+    isOuter: 0,
+    lotterCode: 'cqssc',
+    lotterName: '重庆时时彩',
+    realCategory: 'ssc',
+    status: 0
+  }, {
+    isOuter: 0,
+    lotterCode: 'sdsyxw',
+    lotterName: '山东11选5',
+    realCategory: 'syx5',
+    status: 0
+  }, {
+    isOuter: 0,
+    lotterCode: 'bjpks',
+    lotterName: '北京PK拾',
+    realCategory: 'pk10',
+    status: 0
+  }, {
+    isOuter: 0,
+    lotterCode: 'bjklb',
+    lotterName: '北京快乐8',
+    realCategory: 'kl8',
+    status: 0
+  }, {
+    isOuter: 0,
+    lotterCode: 'xdlks',
+    lotterName: '新德里快3',
+    realCategory: 'k3',
+    status: 0
+  }, {
+    isOuter: 0,
+    lotterCode: 'jzdklb',
+    lotterName: '济州岛快乐8',
+    realCategory: 'kl8',
+    status: 0
+  }
+]
 
 export const setLoginStatus = (status) => {
   return {
@@ -15,7 +55,22 @@ export const setActiveAccount = (data) => {
   }
 }
 
-export const SetCustomizeLottery = createAction(
+export const setActiveUsualLot = createAction(
+  'SET_ACTIVE_USUAL_LOT',
+  async (data) => {
+    let old = await AsyncStorage.getItem('usualLottery')
+    if (data.custom) {
+      AsyncStorage.setItem('usualLottery', JSON.stringify(data.data))
+      return data.data
+    } else {
+      let d = JSON.parse(old) || usual
+      AsyncStorage.setItem('usualLottery', JSON.stringify(d))
+      return d
+    }
+  }
+)
+
+export const setCustomizeLottery = createAction(
   'SET_CUSTOMIZE_LOTTERY',
   async () => {
     let res = await getSysLottery()
--- screens/AddCustomizeGames/AddCustomizeGames.js
+++ screens/AddCustomizeGames/AddCustomizeGames.js
@@ -6,8 +6,12 @@ import {
   RefreshControl, StyleSheet, Image
 } from 'react-native';
 import { Accordion, Flex } from '@ant-design/react-native';
-import Header from './../../components/Header'
+import Header from './../../components/Header';
 import { connect } from "react-redux";
+import {
+  setActiveUsualLot
+} from '../../actions/common'
+import { pull } from 'lodash'
 
 class AddCustomizeGamesScreen extends React.Component {
   static navigationOptions = ({ navigation, navigationOptions }) => {
@@ -30,28 +34,90 @@ class AddCustomizeGamesScreen extends React.Component {
       refreshing: false,
       activeSections: [0],
       checkAllGroup: [],
-      loNumber: {
-        ssc: 0,
-        syx5: 0,
-        pk10: 0,
-        k3: 0,
-        kl8: 0,
-        xyc: 0,
-        kl10: 0,
-        dpc: 0
-      },
+      loNumber: { ssc: 0, syx5: 0, pk10: 0, k3: 0, kl8: 0, xyc: 0, kl10: 0, dpc: 0 },
+    };
+    this.getIconName = value => {
+      switch (value) {
+        case 'ssc':
+          return require('./../../assets/images/home/ssc_icon.png')
+        break
+        case 'syx5':
+          return require('./../../assets/images/home/syxw_icon.png')
+          break
+        case 'kl8':
+          return require('./../../assets/images/home/klc_icon.png')
+          break
+        case 'pk10':
+          return require('./../../assets/images/home/pks_icon.png')
+          break
+        case 'k3':
+          return require('./../../assets/images/home/ks_icon.png')
+          break
+        case 'kl10':
+          return require('./../../assets/images/home/kls_icon.png')
+          break
+        case 'xyc':
+          return require('./../../assets/images/home/xyc_icon.png')
+          break
+        case 'dpc':
+          return require('./../../assets/images/home/dpc_icon.png')
+          break
+        default:
+          return require('./../../assets/images/home/ssc_icon.png')
+      }
     };
     this.onChange = activeSections => {
       this.setState({ activeSections });
     };
   }
 
+  initUsualGames = () => {
+    let { usualLottery } = this.props
+    let loNumber = { ssc: 0, syx5: 0, pk10: 0, k3: 0, kl8: 0, xyc: 0, kl10: 0, dpc: 0 }
+    let checkAllGroup = []
+    usualLottery.forEach(item => {
+      loNumber[item.realCategory] += 1
+      checkAllGroup.push(item.lotterCode)
+    })
+    this.setState({
+      loNumber,
+      checkAllGroup
+    })
+  }
+
   finishAdd = () => {
+    let { checkAllGroup } = this.state
+    let { sysSortLottery } = this.props
+    let sourceData = []
+    let formateUsualData = []
+    sysSortLottery.forEach(item => sourceData.push(...item.originLot,...item.gpLot))
+    sourceData.forEach(item => checkAllGroup.includes(item.lotterCode) ? formateUsualData.push(item) : '')
+    this.props.setActiveUsualLot({custom: 1, data: formateUsualData})
     this.props.navigation.goBack()
   }
 
-  _toSetLot = () => {
-
+  _toSetLot = (v) => {
+    let {realCategory, lotterCode} = v
+    if (this.state.checkAllGroup.includes(lotterCode)) {
+      this.setState((prevState) => {
+        let checkAllGroup = pull(prevState.checkAllGroup, lotterCode)
+        let loNumber = prevState.loNumber
+        loNumber[realCategory] -= 1
+        return { checkAllGroup, loNumber}
+      })
+    } else {
+      if (this.state.checkAllGroup.length >= 9) {
+        alert('最多设置9个')
+        return
+      }
+      this.setState((prevState) => {
+        let checkAllGroup = prevState.checkAllGroup
+        let loNumber = prevState.loNumber
+        checkAllGroup.push(lotterCode)
+        loNumber[realCategory] += 1
+        return { checkAllGroup, loNumber}
+      })
+    }
   }
 
   _onRefresh = () => {
@@ -62,27 +128,31 @@ class AddCustomizeGamesScreen extends React.Component {
   }
 
   componentDidMount() {
+    this.initUsualGames()
     this.props.navigation.setParams({ finishAdd: this.finishAdd })
   }
 
   _renderHeader = item => {
+    let { loNumber } = this.state
     return (
       <Flex style={{width: '95%'}}>
         <View>
-          <Image source={require('./../../assets/images/home/ssc_icon.png')} resizeMode={'contain'} style={styles.coItemImg} />
+          <Image source={this.getIconName(item.realCategory)} resizeMode={'contain'} style={styles.coItemImg} />
         </View>
         <View>
           <Text>
             <Text style={styles.coItemTitle}>{item.categoryName}</Text>
           </Text>
         </View>
-        <View style={styles.coItemText}><Text style={styles.coItemTextCont}>8</Text></View>
+        {loNumber[item.realCategory] !== 0 && <View style={styles.coItemText}><Text style={styles.coItemTextCont}>{loNumber[item.realCategory]}</Text></View>
+        }
       </Flex>
     )
   };
 
   render() {
     let { sysSortLottery } = this.props
+    let { checkAllGroup } = this.state
     return (
       <View style={styles.customContainer}>
         <ScrollView
@@ -110,9 +180,11 @@ class AddCustomizeGamesScreen extends React.Component {
                               return (
                                 <View
                                   key={i}
-                                  style={styles.defaultBtn}
-                                  onPress={() => this._toSetLot(v)}>
-                                  <Text numberOfLines={1} style={styles.defaultBtnText}>{v.lotterName}</Text>
+                                  style={checkAllGroup.includes(v.lotterCode) ? styles.activeBtn : styles.defaultBtn}>
+                                  <Text
+                                    numberOfLines={1}
+                                    style={checkAllGroup.includes(v.lotterCode) ? styles.activeBtnText : styles.defaultBtnText}
+                                    onPress={() => this._toSetLot(v)}>{v.lotterName}</Text>
                                 </View>
                               )
                             })
@@ -127,9 +199,11 @@ class AddCustomizeGamesScreen extends React.Component {
                               return (
                                 <View
                                   key={i}
-                                  style={styles.defaultBtn}
-                                  onPress={() => this._toSetLot(v)}>
-                                  <Text numberOfLines={1} style={styles.defaultBtnText}>{v.lotterName}</Text>
+                                  style={checkAllGroup.includes(v.lotterCode) ? styles.activeBtn : styles.defaultBtn}>
+                                  <Text
+                                    numberOfLines={1}
+                                    style={checkAllGroup.includes(v.lotterCode) ? styles.activeBtnText : styles.defaultBtnText}
+                                    onPress={() => this._toSetLot(v)}>{v.lotterName}</Text>
                                 </View>
                               )
                             })
@@ -174,6 +248,7 @@ const styles = StyleSheet.create({
   },
   lotTitle: {
     marginTop: 10,
+    marginBottom: 5,
     fontSize: 14,
     marginLeft: 10,
   },
@@ -185,9 +260,24 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     backgroundColor: '#e4e4e4'
   },
+  activeBtn: {
+    width: 80,
+    height: 25,
+    marginLeft: 8,
+    marginBottom: 10,
+    borderRadius: 5,
+    backgroundColor: '#016fca'
+  },
   defaultBtnText: {
     fontSize: 12,
     color: '#333333',
+    lineHeight: 20,
+    textAlign: 'center',
+    padding: 3,
+  },
+  activeBtnText: {
+    fontSize: 12,
+    color: '#ffffff',
     lineHeight: 20,
     textAlign: 'center',
     padding: 3,
@@ -201,7 +291,14 @@ const mapStateToProps = (state) => {
     usualLottery
   })
 }
-
+const mapDispatchToProps = (dispatch) => {
+  return {
+    setActiveUsualLot: (data) => {
+      dispatch(setActiveUsualLot(data))
+    }
+  }
+}
 export default connect(
-  mapStateToProps
+  mapStateToProps,
+  mapDispatchToProps
 )(AddCustomizeGamesScreen)
