c48c4b318a25c2eb0bb6bb4bdaa7b2fe763b4ff8
ddi <ddili6599@gmail.com>
2019-01-04 17:45:25 +0800

    find

--- screens/Found/FoundScreen.js
+++ screens/Found/FoundScreen.js
@@ -1,15 +1,122 @@
 import React from 'react'
-import {View, Text, StyleSheet} from 'react-native'
+import {View, Text, StyleSheet, ImageBackground, ScrollView, Image } from 'react-native'
+import { WhiteSpace, Flex } from '@ant-design/react-native';
+import Header from './../../components/Header'
 
 export default class FoundScreen extends React.Component {
   static navigationOptions = {
-    title: '发现'
+    header: <Header hideLeft={true}/>
+  }
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      list: [
+        {
+          title: '开奖公告',
+          text: '实时公布开奖号',
+          route: '',
+          src: require('./../../assets/images/found/open.png')
+        },
+        {
+          title: '优惠活动',
+          text: '回馈新老客户',
+          route: '',
+          src: require('./../../assets/images/found/activity.png')
+        },
+        {
+          title: '趣味游戏',
+          text: '趣味休闲小游戏',
+          route: '',
+          src: require('./../../assets/images/found/game.png')
+        },
+        {
+          title: '积分商城',
+          text: '小惊喜大回报',
+          route: '',
+          src: require('./../../assets/images/found/shop.png')
+        },
+        {
+          title: '幸运选号',
+          text: '狠狠提高中奖率',
+          route: '',
+          src: require('./../../assets/images/found/lucky.png')
+        },
+        {
+          title: '应用分享',
+          text: '与朋友分享娱乐',
+          route: '',
+          src: require('./../../assets/images/found/share.png')
+        },
+        {
+          title: '抢红包',
+          text: '手气旺抢红包',
+          route: '',
+          src: require('./../../assets/images/found/packet.png')
+        }
+      ]
+    }
+  }
+
+  _actionFun = (item) =>{
+    console.log(item)
   }
 
   render() {
+    let { list } = this.state
     return (
       <View style={styles.container}>
-        <Text>我是发现页面</Text>
+        <View style={styles.findHeader}>
+          <ImageBackground source={require('./../../assets/images/found/find_bg.png')} style={{width: '100%', height: '100%'}}>
+            <Flex justify="center">
+              <View style={styles.findHeaderContent}>
+                <Text style={styles.findHeadText}>天祥国际累积派发奖金</Text>
+                <Flex justify="center">
+                  <View>
+                    <Text style={styles.findHeadNumber}>¥</Text>
+                  </View>
+                  <View>
+                    <Text style={styles.findHeadNumber}>563,</Text>
+                  </View>
+                  <View>
+                    <Text style={styles.findHeadNumber}>193,</Text>
+                  </View>
+                  <View>
+                    <Text style={styles.findHeadNumber}>653,</Text>
+                  </View>
+                  <View>
+                    <Text style={styles.findHeadNumber}>245</Text>
+                  </View>
+                </Flex>
+              </View>
+            </Flex>
+          </ImageBackground>
+        </View>
+        <WhiteSpace size="md" />
+        <ScrollView>
+          <Flex wrap="wrap">
+            {
+              list.map((item, index) => {
+                return (
+                  <View style={styles.findItem} key={index} onPress={() => this._actionFun(item)}>
+                    <Flex style={{height: 70}}>
+                      <View style={styles.listBlock}>
+                        <Text style={styles.listTitle}>{item.title}</Text>
+                        <Text style={styles.listText}>{item.text}</Text>
+                      </View>
+                      <View>
+                        <Image
+                          source={item.src}
+                          resizeMode={'contain'}
+                          style={{width: 60, marginLeft: 10}}/>
+                      </View>
+                    </Flex>
+                  </View>
+                )
+              })
+            }
+          </Flex>
+        </ScrollView>
       </View>
     )
   }
@@ -18,7 +125,61 @@ export default class FoundScreen extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 15,
-    backgroundColor: '#fff'
+    backgroundColor: '#f0f0f0'
+  },
+  findHeader: {
+    height: 94
+  },
+  findHeaderContent: {
+    width: '90%',
+    height: 86,
+    borderRadius: 10,
+    backgroundColor: '#e7f4ff',
+    marginTop: 8,
+    //以下是阴影属性：
+    shadowOffset: {width: 0, height: 5},
+    shadowOpacity: 0.5,
+    shadowRadius: 5,
+    shadowColor: '#d7d6d6',
+    //注意：这一句是可以让安卓拥有灰色阴影
+    elevation: 4,
+  },
+  findHeadText: {
+    color: '#016fca',
+    fontSize: 13,
+    paddingTop: 10,
+    paddingBottom: 10,
+    textAlign: 'center',
+  },
+  findHeadNumber: {
+    fontSize: 24,
+    color: '#016fca',
+  },
+  findItem: {
+    width: '45%',
+    marginLeft: '3%',
+    borderWidth: 1,
+    backgroundColor: '#ffffff',
+    borderColor: '#d7d6d6',
+    borderRadius: 10,
+    marginBottom: 10,
+    //以下是阴影属性：
+    shadowOffset: {width: 1, height: 2},
+    shadowOpacity: 0.2,
+    shadowRadius: 5,
+    shadowColor: '#f4f3f3',
+    //注意：这一句是可以让安卓拥有灰色阴影
+    elevation: 1,
+  },
+  listBlock: {
+    paddingLeft: 10
+  },
+  listTitle: {
+    fontSize: 14,
+    color: '#333333'
+  },
+  listText: {
+    fontSize: 10,
+    color: '#8d8b8e'
   }
 })
