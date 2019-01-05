ef38f3aba5ee48662b62c80075229462b64ccbcf
jason <jasonma886@gmail.com>
2019-01-05 11:29:35 +0800

    开户中心

--- screens/Personal/Agent/OpenCenter.js
+++ screens/Personal/Agent/OpenCenter.js
@@ -1,16 +1,32 @@
-import React from 'react'
+import React, {PureComponent} from 'react'
 import {View, Text, StyleSheet} from 'react-native'
 import {connect} from 'react-redux'
 import {SegmentedControl, InputItem, Flex, Button, Toast, PickerView} from '@ant-design/react-native'
 import {Picker} from 'native-base'
 import {addDown, addSignup} from '../../../api/member'
+import UIListView from '../../../components/UIListView'
 
 const TableRow = 20
-const DATE_LENGTH = [
-  {value: 1, label: '1天'},
-  {value: 1, label: '3天'},
-  {value: 1, label: '7天'},
-]
+
+class FlatListItem extends PureComponent {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    let {item, index} = this.props
+    let {balanceHours, createTime} = item
+    return (
+      <View style={{padding: 10}}>
+        <Text style={{color: 'black'}}>序号: {index}</Text>
+        <Text>RowID: {balanceHours}</Text>
+        <Text note>Data: {createTime}</Text>
+      </View>
+    )
+  }
+
+}
+
 class OpenCenter extends React.Component {
   static navigationOptions = {
     title: '开户中心'
@@ -19,12 +35,18 @@ class OpenCenter extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
+      KeyName: 'OpenCenter',
       selectedIndex: 0,
       api: '/user/signupLinkList',
+      params: {
+        pageSize: 10,
+        pageNumber: 1
+      },
       userName: '',
       userRebate: '',
       isProxy: 0,
-      availableTimes: ''
+      availableTimes: '',
+      validDays: 24
     }
   }
 
@@ -36,7 +58,7 @@ class OpenCenter extends React.Component {
   _addDown = () => {
     let {userName, userRebate, isProxy} = this.state
     let {systemMaxRebate, systemMinRebate, userRebateVO} = this.props.rebateInfo
-    maxRebate = userRebateVO[0].userRebate < systemMaxRebate ? userRebateVO[0].userRebate : systemMaxRebate
+    let maxRebate = userRebateVO[0].userRebate < systemMaxRebate ? userRebateVO[0].userRebate : systemMaxRebate
     if (!userName) {
       Toast.info(`请输入账号`)
       return false
@@ -49,8 +71,8 @@ class OpenCenter extends React.Component {
       Toast.info('请输入大小写字母开头，5-10个字符')
       return false
     }
-    if (userRebate > systemMaxRebate) {
-      Toast.info(`返点数不能大于${systemMaxRebate}`)
+    if (userRebate > maxRebate) {
+      Toast.info(`返点数不能大于${maxRebate}`)
       return false
     }
     if (userRebate < systemMinRebate) {
@@ -70,6 +92,50 @@ class OpenCenter extends React.Component {
       this.setState({
         userName: '',
         userRebate: ''
+      })
+    })
+  }
+
+  _addLinkup = () => {
+    let reg = /^\+?[1-9]\d*$/
+    let {userRebate, availableTimes, isProxy, validDays} = this.state
+    let {systemMaxRebate, systemMinRebate, userRebateVO} = this.props.rebateInfo
+    let maxRebate = userRebateVO[0].userRebate < systemMaxRebate ? userRebateVO[0].userRebate : systemMaxRebate
+    if (!availableTimes) {
+      Toast.info(`请输入使用次数`)
+      return false
+    }
+    if (!userRebate) {
+      Toast.info(`请输入彩票返点`)
+      return false
+    }
+    if (!reg.test(availableTimes)) {
+      Toast.info(`使用次数必须是正整数`)
+      return false
+    }
+    if (availableTimes > 9999) {
+      Toast.info(`使用次数不能大于9999`)
+      return false
+    }
+    if (userRebate > maxRebate) {
+      Toast.info(`返点数不能大于${maxRebate}`)
+      return false
+    }
+    if (userRebate < systemMinRebate) {
+      Toast.info(`返点数不能小于${systemMinRebate}`)
+      return false
+    }
+    addSignup({
+      times: availableTimes,
+      isProxy: isProxy,
+      validDays: validDays,
+      rebate: userRebate
+    }).then(res => {
+      Toast.info(res.message)
+      this.setState({
+        userRebate: '',
+        availableTimes: '',
+        validDays: 24
       })
     })
   }
@@ -136,7 +202,7 @@ class OpenCenter extends React.Component {
           </Flex>
           <Flex direction={'row'} style={styles.userType}>
             <Text style={{fontSize: 16}}>链接有效期</Text>
-            <Picker mode="dropdown" style={{width: 180, height: 30, marginLeft: 15, marginRight: 20}}>
+            <Picker selectedValue={this.state.validDays} onValueChange={value => this.setState({validDays: value})} mode="dropdown" style={{width: 180, height: 30, marginLeft: 15, marginRight: 20}}>
               <Picker.Item label="1天" value={24} />
               <Picker.Item label="3天" value={24 * 3} />
               <Picker.Item label="7天" value={ 24 * 7} />
@@ -173,7 +239,7 @@ class OpenCenter extends React.Component {
             彩票返点
           </InputItem>
         </View>
-        <Button size={'large'} type={'primary'} onPress={this._addDown}>生成链接</Button>
+        <Button size={'large'} type={'primary'} onPress={this._addLinkup}>生成链接</Button>
         <View style={{marginTop: 20}}>
           <Text style={styles.hint}>温馨提示</Text>
           <Text style={styles.hint}>生成链接不会立即扣减配额，只有用户使用该链接注册成功的时候，才会扣减配额；</Text>
@@ -184,12 +250,49 @@ class OpenCenter extends React.Component {
   }
 
   manageRender = () => {
+    let {api, params, KeyName, isShow} = this.state
     return (
-      <View>
-        <Text>link manage</Text>
+      <View style={styles.normal}>
+        <UIListView
+          ref={ref => this.OpenCenter = ref}
+          api={api}
+          KeyName={`KeyName-${KeyName}`}
+          params={params}
+          renderItem={this.renderItem}
+          // 第一个参数 params 第二个子组件的将要请求的第N页
+          beforeHttpGet={async ({params, page}, fn) => {
+            // 解决父级数据数据源同步问题，然后数据给到子组件本身
+            await this.setState({
+              params: Object.assign({}, params, {
+                pageNumber: page
+              })
+            })
+            let handlerParams = this.state.params
+            fn(handlerParams, true)
+          }}
+          // 返回数据空或者处理后的数据源
+          beforeUpdateList={({res}, fn) => {
+            console.log(res)
+            let dataList = res.data && res.data.root ? res.data.root : []
+            let {page, records, totalCount} = res.data
+            let NullData = Math.ceil(totalCount / page) < records
+            // 或在这里增加 其他状态码的处理Alter
+            fn({dataList})
+          }}
+        />
       </View>
     )
   }
+
+  renderItem = (item, index) => {
+    return (
+      <FlatListItem
+        item={item}
+        index={index}
+        />
+    )
+  }
+
   render () {
     let contain = ''
     switch (this.state.selectedIndex) {
