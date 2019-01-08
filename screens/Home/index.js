2dc2da1a13873dc09ba25beba7d1b2ae78d1207c
ddi <ddili6599@gmail.com>
2019-01-08 14:37:49 +0800

    delete test

--- screens/Home/index.js
+++ screens/Home/index.js
@@ -1,12 +1,10 @@
 import MailboxScreen from './MailboxScreen'
 import BroadcastScreen from './BroadcastScreen'
 import AddCustomizeGamesScreen from './AddCustomizeGames'
-import MailText from './MailTest'
 
 export default {
   Broadcast: BroadcastScreen,
   CustomizeGames: AddCustomizeGamesScreen,
-  Mailbox: MailboxScreen,
-  MailText
+  Mailbox: MailboxScreen
 }
 
