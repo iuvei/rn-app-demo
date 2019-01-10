5984b0eb0473273e0404c4bed598877aa269f8e4
jason <jasonma886@gmail.com>
2019-01-10 14:48:21 +0800

    个人彩票报表

--- screens/Personal/Myself/LotteryReport.js
+++ screens/Personal/Myself/LotteryReport.js
@@ -1,40 +1,168 @@
-import React from 'react'
-import {View, Text, StyleSheet} from 'react-native'
+import React, {PureComponent} from 'react'
+import {View, Text, StyleSheet, TouchableHighlight} from 'react-native'
+import UIListView from '../../../components/UIListView'
+import {WingBlank, Flex, InputItem} from '@ant-design/react-native'
+import QueryDate from '../../../components/QueryDate'
+import dayjs from 'dayjs'
+import QueryPickerOne from '../../../components/QueryPickerOne'
+import {toFixed4} from '../../../utils/MathUtils'
 
+const GAME_TYPE = [
+  {
+    label: '彩票',
+    id: 1,
+    value: 0
+  }, {
+    label: '快乐彩',
+    id: 2,
+    value: 1
+  }
+]
 
-const TableRow = 20
-export default class PersonalScreen extends React.Component {
+class FlatListItem extends PureComponent {
+  render () {
+    let {
+      userName,
+      lotterName,
+      lotterCode,
+      profitLoss,
+      purchaseAmount,
+      rebateAmount,
+      withdrawAmount,
+      bonus,
+      bettingAmount,
+      teamRebateAmount,
+      isSub,
+    } = this.props.item
+    return (
+      <View style={styles.table}>
+        <Flex><Text style={styles.capital}>{lotterName}</Text></Flex>
+        <Flex direction={'row'} justify={'space-between'}>
+          <View style={styles.column}>
+            <Text style={styles.title}>团队返点:</Text>
+            <Text style={styles.value}>{teamRebateAmount}</Text>
+          </View>
+          <View style={styles.column}>
+            <Text style={styles.title}>盈亏:</Text>
+            <Text style={styles.value}>{toFixed4(profitLoss)}</Text>
+          </View>
+        </Flex>
+        <Flex direction={'row'} justify={'space-between'}>
+          <View style={styles.column}>
+            <Text style={styles.title}>代购额:</Text>
+            <Text style={styles.value}>{toFixed4(purchaseAmount)}</Text>
+          </View>
+          <View style={styles.column}>
+            <Text style={styles.title}>个人返点:</Text>
+            <Text style={styles.value}>{toFixed4(rebateAmount)}</Text>
+          </View>
+        </Flex>
+        <Flex direction={'row'} justify={'space-between'}>
+          <View style={styles.column}>
+            <Text style={styles.title}>撤单:</Text>
+            <Text style={styles.value}>{toFixed4(withdrawAmount)}</Text>
+          </View>
+          <View style={styles.column}>
+            <Text style={styles.title}>奖金:</Text>
+            <Text style={styles.value}>{toFixed4(bonus)}</Text>
+          </View>
+        </Flex>
+        <Flex direction={'row'} justify={'space-between'}>
+          <View style={styles.column}>
+            <Text style={styles.title}>投注:</Text>
+            <Text style={styles.value}>{toFixed4(bettingAmount)}</Text>
+          </View>
+          <View style={styles.column}>
+          </View>
+        </Flex>
+      </View>
+    )
+  }
+}
+
+export default class LotteryReport extends React.Component {
   static navigationOptions = {
-    title: '个人彩票报表'
+    title: '彩票报表'
   }
 
-  constructor(props) {
+  constructor (props) {
     super(props)
+    let end = dayjs().format('YYYY-MM-DD')
+    let start = dayjs().subtract(1, 'day').format('YYYY-MM-DD')
     this.state = {
-      KeyName: 'BetHistory',
-      api: '/order/getOrderStatistics',
+      KeyName: 'LotteryReport',
+      api: '/frontReport/reportform/getUserLotteryReport',
+      isShow: false,
       params: {
-        userId: '',
-        orderId: '',
-        proxyType: 0, // 0自己、1直接下级、2所有下级，默认0
-        orderType: 0, // 0彩票,1游戏
-        orderIssue: '', // 期号
-        lotterCode: '', // 必传
-        startTime: '2018-12-13',
-        endTime: '2019-01-01',
-        status: '',
+        lotterCode: '',
         pageNumber: 1,
-        isAddition: 0, // 是否追号：0 否、1 是
         pageSize: 10,
-        isOuter: '' // 0 否 1 是
+        reportType: 0,
+        startTime: start,
+        endTime: end
       }
     }
   }
 
-  render() {
+  renderItem = (item, index) => {
+    return (
+      <FlatListItem
+        item={item}
+        index={index}/>
+    )
+  }
+
+  onSearch = async () => {
+    await this.setState({isShow: true}, () => {
+      this.setState({isShow: false})
+    })
+  }
+
+  handleDate = async ({startTime, endTime}) => {
+    await this.setState(prevState => ({
+      params: {...prevState.params, startTime, endTime}
+    }))
+    this.onSearch()
+  }
+
+  handlePickerBack = (val) => {
+    this.setState(prevState => ({
+      params: {...prevState.params, ...val}
+    }))
+  }
+
+  render () {
+    let {api, params, KeyName, isShow} = this.state
     return (
       <View style={styles.container}>
-        <Text>hello personal</Text>
+        <WingBlank>
+          <QueryDate handleDate={this.handleDate}/>
+          <Flex style={{marginTop: 6}} direction={'row'}>
+            <Flex.Item>
+              <QueryPickerOne
+                data={GAME_TYPE}
+                queryName={'reportType'}
+                handlePickerBack={this.handlePickerBack}/>
+            </Flex.Item>
+            <Flex.Item></Flex.Item>
+            <Flex.Item></Flex.Item>
+          </Flex>
+        </WingBlank>
+        {isShow ? null :
+          <UIListView
+            ref={ref => this.LotteryReport = ref}
+            api={api}
+            KeyName={`KeyName-${KeyName}`}
+            params={params}
+            renderItem={this.renderItem}
+            beforeUpdateList={({res}, fn) => {
+              let dataList = res.data && res.data.pageColumns ? res.data.pageColumns : []
+              let {currentPage, total} = res.data.pageInfo
+              let NullData = Math.ceil(total / 10) < currentPage
+              fn(NullData ? [] : {dataList})
+            }}
+          />
+        }
       </View>
     )
   }
@@ -43,7 +171,28 @@ export default class PersonalScreen extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 15,
     backgroundColor: '#fff'
+  },
+  table: {
+    padding: 10
+  },
+  capital: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'gold'
+  },
+  title: {
+    fontSize: 16,
+    color: '#999'
+  },
+  value: {
+    fontSize: 16,
+    color: '#333'
+  },
+  column: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    flex: 1,
+    marginRight: 8
   }
 })
