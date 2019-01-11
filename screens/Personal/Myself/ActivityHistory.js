c996e2cf2a3fbacb9d73f012cab0ca8af3fb036b
wade <wadeflash123@gmail.com>
2019-01-11 10:31:33 +0800

    活动记录

--- screens/Personal/Myself/ActivityHistory.js
+++ screens/Personal/Myself/ActivityHistory.js
@@ -1,40 +1,158 @@
 import React from 'react'
-import {View, Text, StyleSheet} from 'react-native'
-
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableHighlight
+} from 'react-native'
+import {
+  Flex,
+  Button
+} from '@ant-design/react-native'
+import UIListView from '../../../components/UIListView'
+import QueryDate from '../../../components/QueryDate'
+import QueryPickerOne from '../../../components/QueryPickerOne'
+import { ActivityStatus } from '../../../data/options'
 
 const TableRow = 20
-export default class PersonalScreen extends React.Component {
-  static navigationOptions = {
-    title: '活动记录'
+
+class FlatListItem extends React.PureComponent {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    let {item, index} = this.props
+    let {activityName, activityBonus, activitytTime, allConsumeQuota, bonus, distributeCurrency, joinCurrency, nowConsumeQuota, status} = item
+    return (
+      <TouchableHighlight onPress={() => this.props.onPressFunc(item)}>
+        <View style={{padding: 10, backgroundColor: '#fff', position: 'relative'}}>
+          <Text style={{fontSize: 15, lineHeight: 24}}>{activityName}</Text>
+          <Flex>
+            <Text style={{width: 150, color: '#666', fontSize: 14, lineHeight: 22}}>活动时间: <Text style={{color: '#1689e6'}}>{activitytTime}</Text></Text>
+            <Text style={{width: 150, color: '#666', fontSize: 14, lineHeight: 22}}>{status}</Text>
+          </Flex>
+          <Text style={{color: '#666', fontSize: 14, lineHeight: 22}} note>活动参与金额: <Text style={{color: '#1689e6'}}>{activityBonus}</Text></Text>
+          <Text style={{color: '#666', fontSize: 14, lineHeight: 22}} note>当前限制: <Text style={{color: '#1689e6'}}>{nowConsumeQuota}</Text></Text>
+        </View>
+      </TouchableHighlight>
+    )
+  }
+}
+
+export default class ActivityHistory extends React.Component {
+  static navigationOptions = ({ navigation, navigationOptions }) => {
+    return {
+      title: '活动记录',
+      headerRight: <Button style={{marginRight: 14}} type="ghost" size="small" onPress={navigation.getParam('onSearch')}>
+        <Text style={{color: '#fff'}}>查询</Text>
+      </Button>
+    }
   }
 
   constructor(props) {
     super(props)
     this.state = {
-      KeyName: 'BetHistory',
-      api: '/order/getOrderStatistics',
+      KeyName: 'ActivityHistory',
+      api: '/frontReport/getActivityRecordStatistics',
       params: {
         userId: '',
-        orderId: '',
-        proxyType: 0, // 0自己、1直接下级、2所有下级，默认0
-        orderType: 0, // 0彩票,1游戏
-        orderIssue: '', // 期号
-        lotterCode: '', // 必传
-        startTime: '2018-12-13',
-        endTime: '2019-01-01',
-        status: '',
+        startTime: '',
+        endTime: '',
         pageNumber: 1,
-        isAddition: 0, // 是否追号：0 否、1 是
+        status: '',
         pageSize: 10,
-        isOuter: '' // 0 否 1 是
+        activityName: ''
       }
     }
   }
 
+  componentDidMount() {
+    this.props.navigation.setParams({ onSearch: this.onSearch })
+  }
+
+  handleDate = ({startTime, endTime}) => {
+    this.setState(prevState => ({
+      params: Object.assign({}, {...prevState.params, startTime, endTime})
+    }))
+  }
+
+  // renderItem
+  // item, index, separators
+  renderItem = (item, index) => {
+    return (
+      <FlatListItem
+        item={item}
+        index={index}
+        onPressFunc={(Item) => {
+          this.onPressItem(Item)
+        }}/>
+    )
+  }
+
+  // 点击单元表格
+  onPressItem = (item) => { 
+    // 点击一项改变数据重置数据
+    // let Row = this.BetHistory.listView.getRows().slice()
+    // Row.find(rows => rows.orderId === item.orderId).ruleName = '自定义'
+    // this.BetHistory.listView.updateDataSource(Row)
+  }
+
+  onSearch = async () => {
+    this.ActivityHistory.listView.refresh()
+  }
+
+  handlePickerBack = (val) => {
+    this.setState(prevState => ({
+      params: {...prevState.params, ...val}
+    }))
+  }
+
+
   render() {
+    let {api, params, KeyName} = this.state
+
     return (
       <View style={styles.container}>
-        <Text>hello personal</Text>
+        <View>
+            <QueryDate handleDate={this.handleDate}/>
+            <View>
+              <Flex justify="between" style={{height: 30}}>
+                <Flex.Item>
+                  <QueryPickerOne
+                    data={ActivityStatus}
+                    queryName={'status'}
+                    handlePickerBack={this.handlePickerBack}/>
+                </Flex.Item>
+              </Flex>
+            </View>
+        </View>
+        <UIListView
+          ref={ref => this.ActivityHistory = ref}
+          api={api}
+          KeyName={`KeyName-${KeyName}`}
+          params={params}
+          renderItem={this.renderItem}
+          // 第一个参数 params 第二个子组件的将要请求的第N页
+          // beforeHttpGet={async ({params, page}, fn) => {
+          //   // 解决父级数据数据源同步问题，然后数据给到子组件本身
+          //   await this.setState({
+          //     params: Object.assign({}, params, {
+          //       pageNumber: page
+          //     })
+          //   })
+          //   let handlerParams = this.state.params
+          //   fn(handlerParams, true)
+          // }}
+          // 返回数据空或者处理后的数据源
+          beforeUpdateList={({res}, fn) => {
+            let dataList = res.data && res.data.data ? res.data.data : []
+            let {pageNumber, pageSize, totalCount} = res.data
+            let NullData = Math.ceil(totalCount / pageSize) < pageNumber
+            // 或在这里增加 其他状态码的处理Alter
+            fn(NullData ? [] : {dataList})
+          }}
+        />
       </View>
     )
   }
@@ -43,7 +161,13 @@ export default class PersonalScreen extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 15,
-    backgroundColor: '#fff'
+    padding: 10,
+    paddingTop: 5,
+    backgroundColor: '#f0f0f0',
+  },
+  spa: {
+    flex: 1,
+    height: StyleSheet.hairlineWidth,
+    backgroundColor: '#8E8E8E'
   }
 })
