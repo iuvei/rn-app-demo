1841bb29bf1432de23f2484ef5d549e05eb364b1
wade <wadeflash123@gmail.com>
2019-02-25 11:58:11 +0800

    底部icon

--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Platform} from 'react-native'
+import {Platform, Image} from 'react-native'
 import {createStackNavigator, createBottomTabNavigator} from 'react-navigation'
 
 import TabBarIcon from '../components/TabBarIcon'
@@ -8,6 +8,8 @@ import GameScreen from '../screens/Games/GamesScreen'
 import RechargeScreen from '../screens/Recharge/RechargeScreen'
 import FoundScreen from '../screens/Found/FoundScreen'
 import PersonalScreen from '../screens/Personal/PersonalScreen'
+import SvgIcon from '../components/SvgIcon'
+import Colors from '../constants/Colors'
 
 // 首页
 const HomeStack = createStackNavigator({
@@ -17,10 +19,12 @@ const HomeStack = createStackNavigator({
 HomeStack.navigationOptions = {
   tabBarLabel: '首页',
   tabBarIcon: ({focused}) => (
-    <TabBarIcon
-      focused={focused}
-      name={Platform.OS === 'ios' ? "ios-home" : "md-home"}
-    />
+    // <TabBarIcon
+    //   focused={focused}
+    //   name={Platform.OS === 'ios' ? "ios-home" : "md-home"}
+    // />
+    focused ? <Image source={require('../assets/images/footericon/home_selected.png')} style={{resizeMode: 'contain', width: 28, height: 28}}/> :
+    <Image source={require('../assets/images/footericon/home_normal.png')} style={{resizeMode: 'contain', width: 28, height: 28}}/>
   )
 }
 
@@ -29,12 +33,15 @@ const GamesStack = createStackNavigator({
   Links: GameScreen
 })
 GamesStack.navigationOptions = {
-  tabBarLabel: '彩厅',
+  tabBarLabel: '彩厅', // games_room
   tabBarIcon: ({focused}) => (
-    <TabBarIcon
-      focused={focused}
-      name="logo-game-controller-b"
-    />
+    // <TabBarIcon
+    //   focused={focused}
+    //   name="ios-arrow-dropdown"
+    // />
+    // <SvgIcon icon={focused ? 'game' : 'games_room'} size={26} color={focused ? Colors.tabIconSelected : Colors.tabIconDefault}/>
+    focused ? <Image source={require('../assets/images/footericon/game_selected.png')} style={{resizeMode: 'contain', width: 28, height: 28}}/> :
+      <Image source={require('../assets/images/footericon/game_normal_origin.png')} style={{resizeMode: 'contain', width: 28, height: 28}}/>
   )
 }
 
@@ -45,10 +52,12 @@ const RechargeStack = createStackNavigator({
 RechargeStack.navigationOptions = {
   tabBarLabel: '充值',
   tabBarIcon: ({focused}) => (
-    <TabBarIcon
-      focused={focused}
-      name={Platform.OS === 'ios' ? "logo-yen" : "logo-yen"}
-    />
+    // <TabBarIcon
+    //   focused={focused}
+    //   name={Platform.OS === 'ios' ? "logo-yen" : "logo-yen"}
+    // />
+    focused ? <Image source={require('../assets/images/footericon/recharge_select.png')} style={{resizeMode: 'contain', width: 28, height: 28}}/> :
+    <Image source={require('../assets/images/footericon/recharge_normal_origin.png')} style={{resizeMode: 'contain', width: 28, height: 28}}/>
   )
 }
 
@@ -59,10 +68,13 @@ const FoundStack = createStackNavigator({
 FoundStack.navigationOptions = {
   tabBarLabel: '发现',
   tabBarIcon: ({focused}) => (
-    <TabBarIcon
-      focused={focused}
-      name={Platform.OS === 'ios' ? 'ios-search' : 'md-search'}
-    />
+    // <TabBarIcon
+    //   focused={focused}
+    //   name={Platform.OS === 'ios' ? 'ios-search' : 'md-search'}
+    // />
+    // <SvgIcon icon={focused ? 'compass_focused' : 'compass'} size={26} color={focused ? Colors.tabIconSelected : Colors.tabIconDefault}/>
+    focused ? <Image source={require('../assets/images/footericon/Notice_Selected.png')} style={{resizeMode: 'contain', width: 28, height: 28}}/> :
+    <Image source={require('../assets/images/footericon/notice_normal_origin.png')} style={{resizeMode: 'contain', width: 28, height: 28}}/>
   )
 }
 
@@ -73,11 +85,13 @@ const PersonalStack = createStackNavigator({
 PersonalStack.navigationOptions = {
   tabBarLabel: '个人中心',
   tabBarIcon: ({focused}) => (
-    <TabBarIcon
-      focused={focused}
-      tips={true}
-      name={Platform.OS === 'ios' ? 'ios-person' : 'md-person'}
-    />
+    // <TabBarIcon
+    //   focused={focused}
+    //   tips={true}
+    //   name={Platform.OS === 'ios' ? 'ios-person' : 'md-person'}
+    // />
+    focused ? <Image source={require('../assets/images/footericon/centre_selected.png')} style={{resizeMode: 'contain', width: 28, height: 28}}/> :
+    <Image source={require('../assets/images/footericon/centre_normal_origin.png')} style={{resizeMode: 'contain', width: 28, height: 28}}/>
   )
 }
 
