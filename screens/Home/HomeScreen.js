ab93cd7983f04a6c53a13dab624d554633d75769
wade <wadeflash123@gmail.com>
2019-02-26 15:25:02 +0800

    优化

--- screens/Home/HomeScreen.js
+++ screens/Home/HomeScreen.js
@@ -162,7 +162,7 @@ class HomeScreen extends React.Component {
   }
 
   componentWillReceiveProps(np) {
-    if (!np.passwordRule.bandUserPassword) {
+    if (np.passwordRule.bandUserPassword) {
       this.showBindPwd({type: 'login'})
     } else if (np.passwordRule.bandUserPayPassword) {
       this.showBindPwd({type: 'paypwd'})
