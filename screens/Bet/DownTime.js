2429227c4cce1d2383960341aae0b2b598326847
jason <jasonma886@gmail.com>
2019-02-26 15:11:24 +0800

    进度条

--- screens/Bet/DownTime.js
+++ screens/Bet/DownTime.js
@@ -6,6 +6,8 @@ import {
 import { Button } from '@ant-design/react-native'
 import { getIconName } from '../../utils/getLotImg'
 import { stylesUtil } from '../../utils/ScreenUtil'
+// import Progress from '../../components/ProgressBar'
+// import _ from 'lodash'
 
 class DownTime extends Component {
   constructor(props) {
@@ -18,6 +20,18 @@ class DownTime extends Component {
 
   componentDidMount() {
   }
+
+  // componentWillReceiveProps(np) {
+  //   let downTime = this.props.downTime
+  //   if (!_.isEqual(downTime, np.downTime)) {
+  //     let {h1, h2, m1, m2, s1, s2} = np.downTime
+  //     let length = ((h1 * 10 + h2 * 1) * 3600 + (m1 * 10 + m2 * 1) * 60 + s1 * 10 + s2 * 1)
+  //     if (length > 100) {
+  //       length = 100
+  //     }
+  //     this.refs.progressBar.progress = length / 100
+  //   }
+  // }
 
   componentWillUnmount() {
     this.setState = () => () => {
@@ -35,41 +49,44 @@ class DownTime extends Component {
       navParams: {realCategory, lotterNumber}
     } = this.props
     return (
-      <View style={styles.container}>
-        <View style={styles.lotImageView}>
-          <Image
-            source={getIconName(realCategory)}
-            style={styles.lotImage}
-          />
-        </View>
-        <View style={styles.openResultView}>
-          <Text style={styles.openResultText}>
-            <Text style={{color: '#00bbcc'}}>{prevOpenResult.openIssue || '000000000'}</Text>
-            <Text style={styles.stopTimeText}> 期开奖结果</Text>
-          </Text>
-          <View style={styles.openListView}>
-            {
-              openList.length ? openList.map((b, bIdx) =>
-                  <Button
-                    key={`${bIdx}-${b}`}
-                    type="primary" size="small"
-                    style={styles['lotBall' + (lotterNumber || 5)]}>
-                    <Text style={styles['lotBallTxt' + (lotterNumber || 5)]}>{b}</Text>
-                  </Button>
-                ) :
-                <View style={styles.stopTimeView}>
-                  <Text style={styles.stopTimeText}>正在开奖中...</Text>
-                </View>
-            }
+      <View>
+        <View style={styles.container}>
+          <View style={styles.lotImageView}>
+            <Image
+              source={getIconName(realCategory)}
+              style={styles.lotImage}
+            />
           </View>
-          <Text style={styles.stopTimeText}>
-            <Text style={{color: '#00bbcc'}}>{openIssue.currentIssue} </Text>
-            距离封单
-            <Text style={{color: '#f3564d'}}>
-              {` ${h1}${h2}:${m1}${m2}:${s1}${s2}`}
+          <View style={styles.openResultView}>
+            <Text style={styles.openResultText}>
+              <Text style={{color: '#00bbcc'}}>{prevOpenResult.openIssue || '000000000'}</Text>
+              <Text style={styles.stopTimeText}> 期开奖结果</Text>
             </Text>
-          </Text>
+            <View style={styles.openListView}>
+              {
+                openList.length ? openList.map((b, bIdx) =>
+                    <Button
+                      key={`${bIdx}-${b}`}
+                      type="primary" size="small"
+                      style={styles['lotBall' + (lotterNumber || 5)]}>
+                      <Text style={styles['lotBallTxt' + (lotterNumber || 5)]}>{b}</Text>
+                    </Button>
+                  ) :
+                  <View style={styles.stopTimeView}>
+                    <Text style={styles.stopTimeText}>正在开奖中...</Text>
+                  </View>
+              }
+            </View>
+            <Text style={styles.stopTimeText}>
+              <Text style={{color: '#00bbcc'}}>{openIssue.currentIssue} </Text>
+              距离封单
+              <Text style={{color: '#f3564d'}}>
+                {` ${h1}${h2}:${m1}${m2}:${s1}${s2}`}
+              </Text>
+            </Text>
+          </View>
         </View>
+        {/*<Progress ref="progressBar" progressColor="yellow" />*/}
       </View>
     )
   }
