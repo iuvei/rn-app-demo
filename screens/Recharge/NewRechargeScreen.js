e16142f2e4d21b5e8cae09cd2d19ff6ebbc149ec
wade <wadeflash123@gmail.com>
2019-02-27 22:32:57 +0800

    充值

--- screens/Recharge/NewRechargeScreen.js
+++ screens/Recharge/NewRechargeScreen.js
@@ -7,7 +7,8 @@ import InputAmount from './InputAmount'
 import {
   Dimensions,
   View,
-  ScrollView
+  ScrollView,
+  Text
 } from 'react-native'
 
 const height = Dimensions.get('window').height
@@ -26,6 +27,7 @@ class NewRechargeScreen extends React.Component {
     this.state = {
       activeTab: ''
     }
+    props.AsetUserSecureLevel()
   }
 
   tabsChange = (v) =>{
@@ -35,6 +37,31 @@ class NewRechargeScreen extends React.Component {
   }
   
   render () {
+    let { userSecurityLevel, userBankInfo } = this.props
+
+    if (!userSecurityLevel.isTradePassword && userSecurityLevel.rechargeTradpswd) {
+      return (
+        <View style={{backgroundColor: '#fff', paddingVertical: 25}}>
+          <Text style={{color: '#333', textAlign: 'center'}}>暂未设置资金密码，请<Text onPress={this.goSetTrade} style={{color: '#f15a23', fontSize: 15}}>前往设置</Text></Text>
+        </View>
+      )
+    }
+    if (!userSecurityLevel.isBankUserName && userSecurityLevel.rechargeBankName) {
+      return (
+        <View style={{backgroundColor: '#fff', paddingVertical: 25}}>
+          <Text style={{color: '#333', textAlign: 'center'}}>暂未绑定银行卡姓名，请<Text onPress={() => this.props.navigation.navigate('BindBankname')} style={{color: '#f15a23', fontSize: 15}}>前往设置</Text></Text>
+        </View>
+      )
+    }
+    if (userSecurityLevel.rechargeBank && userBankInfo.userBankCards.length === 0
+    && userBankInfo.userBankCards.find(item=>item.status===0) === undefined) {
+      return (
+        <View style={{backgroundColor: '#fff', paddingVertical: 25}}>
+          <Text style={{color: '#333', textAlign: 'center'}}>暂未绑定银行卡/银行卡审核中，请<Text onPress={() => this.props.navigation.navigate('BankManager')} style={{color: '#f15a23', fontSize: 15}}>前往设置</Text></Text>
+        </View>
+      )
+    }
+
     return (
       <View style={{flex: 1}}>
         <AccountsPanel/>
