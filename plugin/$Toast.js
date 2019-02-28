7ac8e9bf4ad695a2c75c52075a745b2695053bd4
jason <jasonma886@gmail.com>
2019-02-28 11:56:44 +0800

    toast 位置

--- plugin/$Toast.js
+++ plugin/$Toast.js
@@ -1,8 +1,23 @@
+import {Toast as NToast} from 'native-base'
 import {Toast} from '@ant-design/react-native'
+import {Platform} from 'react-native'
 
 const $Toast = {
   info: (message) => {
-    Toast.info(message, 1, undefined, false)
+    // Toast.info(message, 1, undefined, false)
+    NToast.show({
+      text: message,
+      buttonText: 'OK',
+      style: {
+        bottom: 500,
+        ...Platform.select({
+          'android': {
+            marginHorizontal: 20,
+            borderRadius: 5
+          }
+        })
+      }
+    })
   },
   success: (message) => {
     Toast.success(message, 1, undefined, false)
