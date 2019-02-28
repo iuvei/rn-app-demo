f9706668159d2a91cade2ee470cac4fac7577f6b
jason <jasonma886@gmail.com>
2019-02-28 17:09:13 +0800

    catch

--- plugin/$Toast.js
+++ plugin/$Toast.js
@@ -3,13 +3,17 @@ import {default as EToast} from 'react-native-root-toast'
 
 const $Toast = {
   info: (message) => {
-    EToast.show(message, {
-      duration: 1000, // toast显示时长
-      position: 100, // toast位置
-      shadow: true, // toast是否出现阴影
-      animation: true, // toast显示/隐藏的时候是否需要使用动画过渡
-      hideOnPress: true, // 是否可以通过点击事件对toast进行隐藏
-      delay: 0})
+    try {
+      EToast.show(message, {
+        duration: 1000, // toast显示时长
+        position: 100, // toast位置
+        shadow: true, // toast是否出现阴影
+        animation: true, // toast显示/隐藏的时候是否需要使用动画过渡
+        hideOnPress: true, // 是否可以通过点击事件对toast进行隐藏
+        delay: 0})
+    } catch (e) {
+      Toast.info(message, 1, undefined, false)
+    }
   },
   success: (message) => {
     Toast.success(message, 1, undefined, false)
